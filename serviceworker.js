1ae22352f76e1f574a4cde794ccb0c4cab1b48ed
KNIGHT Fiona <flk1@staff.staffs.ac.uk>
2017-04-04 09:40:15 +0100

    offline map

--- serviceworker.js
+++ serviceworker.js
@@ -1,54 +1,60 @@
-var CACHE_NAME = 'gih-cache-v5';
+var BASE_PATH = '/proghapi/';
+var CACHE_NAME = 'gih-cache-v6';
 var CACHED_URLS = [
-  // Our HTML
-  'first.html',
-  // Images for favicons
-    'appimages/android-icon-36x36.png',
-    'appimages/android-icon-48x48.png',
-    'appimages/android-icon-72x72.png',
-    'appimages/android-icon-96x96.png',
-    'appimages/android-icon-144x144.png',
-    'appimages/android-icon-192x192.png',
-    'appimages/favicon-32x32.png',
-    //Images for page
+    // Our HTML
+    BASE_PATH + 'first.html',
     
-    'appimages/dino.png',
-  'appimages/jack.jpg',
-  'appimages/paddy.jpg',
-    'appimages/favicon.ico',
-  'appimages/favicon-16x16.png',
-  'appimages/favicon-32x32.png',
-  'appimages/favicon-96x96.png',
-  'appimages/ms-icon-70x70.png',
-  'appimages/ms-icon-144x144.png',
-  'appimages/ms-icon-150x150.png',
-  'appimages/ms-icon-310x310.png',
-  'eventsimages/example-blog01.jpg',
-  'eventsimages/example-blog02.jpg',
-  'eventsimages/example-blog03.jpg',
-  'eventsimages/example-blog04.jpg',
-  'eventsimages/example-blog05.jpg',
-  'eventsimages/example-blog06.jpg',
-  'eventsimages/example-blog07.jpg',
-  'eventsimages/example-work01.jpg',
-  'eventsimages/example-work02.jpg',
-  'eventsimages/example-work03.jpg',
-  'eventsimages/example-work04.jpg',
-  'eventsimages/example-work05.jpg',
-  'eventsimages/example-work06.jpg',
-  'eventsimages/example-work07.jpg',
-  'eventsimages/example-work08.jpg',
-  'eventsimages/example-work09.jpg',  
-  // JavaScript
-    'material.js',
+    // Images for favicons
+    BASE_PATH + 'appimages/android-icon-36x36.png',
+    BASE_PATH + 'appimages/android-icon-48x48.png',
+    BASE_PATH + 'appimages/android-icon-72x72.png',
+    BASE_PATH + 'appimages/android-icon-96x96.png',
+    BASE_PATH + 'appimages/android-icon-144x144.png',
+    BASE_PATH + 'appimages/android-icon-192x192.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+
+    //Images for page
+    BASE_PATH + 'appimages/offlinemap.jpg',
+    BASE_PATH + 'appimages/dino.png',
+    BASE_PATH + 'appimages/jack.jpg',
+    BASE_PATH + 'appimages/paddy.jpg',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'appimages/favicon-96x96.png',
+    BASE_PATH + 'appimages/ms-icon-70x70.png',
+    BASE_PATH + 'appimages/ms-icon-144x144.png',
+    BASE_PATH + 'appimages/ms-icon-150x150.png',
+    BASE_PATH + 'appimages/ms-icon-310x310.png',
+    BASE_PATH + 'eventsimages/example-blog01.jpg',
+    BASE_PATH + 'eventsimages/example-blog02.jpg',
+    BASE_PATH + 'eventsimages/example-blog03.jpg',
+    BASE_PATH + 'eventsimages/example-blog04.jpg',
+    BASE_PATH + 'eventsimages/example-blog05.jpg',
+    BASE_PATH + 'eventsimages/example-blog06.jpg',
+    BASE_PATH + 'eventsimages/example-blog07.jpg',
+    BASE_PATH + 'eventsimages/example-work01.jpg',
+    BASE_PATH + 'eventsimages/example-work02.jpg',
+    BASE_PATH + 'eventsimages/example-work03.jpg',
+    BASE_PATH + 'eventsimages/example-work04.jpg',
+    BASE_PATH + 'eventsimages/example-work05.jpg',
+    BASE_PATH + 'eventsimages/example-work06.jpg',
+    BASE_PATH + 'eventsimages/example-work07.jpg',
+    BASE_PATH + 'eventsimages/example-work08.jpg',
+    BASE_PATH + 'eventsimages/example-work09.jpg',  
+    // JavaScript
+    BASE_PATH + 'offline-map.js',
+    BASE_PATH + 'material.js',
+    // Manifest
+    BASE_PATH + 'manifest.json',
   // CSS and fonts
-    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en',
+    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'min-style.css',
-    'styles.css'
+    BASE_PATH + 'min-style.css',
+    BASE_PATH + 'styles.css'
 ];
 
-var googleMapsAPIJS = 'YOUR_GOOGLE_MAPURL';
+var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCMNj1opBAeNjTEY4PdjCABCwJdrHx0cvI&callback=initMap';
 
 self.addEventListener('install', function(event) {
   // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
@@ -62,7 +68,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
   // Handle requests for index.html
-  if (requestURL.pathname === 'first.html') {
+  if (requestURL.pathname === BASE_PATH + 'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match('first.html').then(function(cachedResponse) {
@@ -74,8 +80,8 @@ self.addEventListener('fetch', function(event) {
         });
       })
     );
-  // Handle requests for Google Maps JavaScript API file
- /* } else if (requestURL.href === googleMapsAPIJS) {
+ // Handle requests for Google Maps JavaScript API file
+  } else if (requestURL.href === googleMapsAPIJS) {
     event.respondWith(
       fetch(
         googleMapsAPIJS+'&'+Date.now(),
@@ -83,10 +89,10 @@ self.addEventListener('fetch', function(event) {
       ).catch(function() {
         return caches.match('offline-map.js');
       })
-    );*/
+    );
   } else if (
-    CACHED_URLS.indexOf(requestURL.href) === -1 ||
-    CACHED_URLS.indexOf(requestURL.pathname) === -1
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)
   ) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
